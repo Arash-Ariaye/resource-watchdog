e24cc50228b7754214fc947eb05871e35351c69c
Arash Ariaye <ghost.a78@gmail.com>
2026-02-27 13:04:40 +0330

    Create resource_watchdog.sh

---
+++ resource_watchdog.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+
+CPU_LIMIT=95
+RAM_LIMIT=95
+MAIN_SERVICE="paqet.service"
+LOG_FILE="/var/log/resource_watchdog.log"
+
+# CPU usage %
+CPU_USAGE=$(top -bn1 | awk -F',' '/Cpu/ {printf "%.0f", 100 - $4}')
+
+# RAM usage %
+TOTAL_RAM=$(free | awk '/Mem:/ {print $2}')
+USED_RAM=$(free | awk '/Mem:/ {print $3}')
+RAM_USAGE=$(awk "BEGIN {printf \"%.0f\", ($USED_RAM/$TOTAL_RAM)*100}")
+
+if [ "$CPU_USAGE" -ge "$CPU_LIMIT" ] || [ "$RAM_USAGE" -ge "$RAM_LIMIT" ]; then
+
+    REASON=""
+    [ "$CPU_USAGE" -ge "$CPU_LIMIT" ] && REASON="CPU=${CPU_USAGE}%"
+    [ "$RAM_USAGE" -ge "$RAM_LIMIT" ] && REASON="${REASON} RAM=${RAM_USAGE}%"
+
+    systemctl restart "$MAIN_SERVICE"
+
+    systemctl list-unit-files | awk '/^backhaul-/ {print $1}' | while read svc; do
+        systemctl restart "$svc"
+    done
+
+    echo "$(date '+%Y-%m-%d %H:%M:%S') | Restarted due to $REASON" >> "$LOG_FILE"
+fi
